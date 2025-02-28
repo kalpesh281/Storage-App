5b634c74da1aa97b1e86e97d793b9346b0164a1a
kalpesh senva <shenvakalpesh4@gmail.com>
2025-03-01 00:25:34 +0530

    frontend setup

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className="flex items-center justify-center min-h-screen bg-gray-900 text-white">
+        <h1 className="text-4xl font-bold">Hello, Tailwind + Vite! 🚀</h1>
+      </div>
+    </>
+  );
+}
+
+export default App
